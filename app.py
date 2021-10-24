ca5cf11d664ccf482c4d15b8c4cfd74928c80227
Rubén Cid <rubencid001@gmail.com>
2021-10-24 16:06:18 +0200

    Añadiendo Algunas Notas: 1

--- app.py
+++ app.py
@@ -22,12 +22,44 @@ app = dash.Dash("metro-kiev-grupo-1")
 
 app.layout = html.Div()
 
+# TODO Crear el Layout
 
 # ------- App Callbacks (FOR MAKING THE APP INTERACTIVE) ------------
 
-@app.callback()
-def crear_path() -> html.Div():
+@app.callbackk()
+def resaltar_origen(origen) -> str:
+    """
+    Debe resaltar el nodo origen en el grafo con un color para diferenciarlo
+    """
+    # TODO Implementar
     pass
+
+
+@app.callback()
+def crear_path_input(origin, destino1, grafo) -> html.Div(), str:
+    """
+    Toma el nodo de inicio del input y el nodo selecionado en el input o  el grafo:
+    :param: origin. Nodo Origen. Obtenido del Input Origen de los nodos
+    :param: destino1. Nodo Destino. Obtenido del Input Destino de los nodos.
+    :param: **kwargs. Quitar si no se usa, solo era para que sepais que podeis añadir más datos
+    """
+    # TODO Implement
+    # Nota: Debe de selecionar el nodo destino en le grafo
+    pass
+
+
+@app.callback()
+def crear_path_grafo(origin, destino1) -> html.Div(), str:
+    """
+    Toma el nodo de inicio del input y el nodo selecionado en el input o  el grafo:
+    :param: origin. Nodo Origen. Obtenido del Input Origen de los nodos
+    :param: destino1. Nodo Destino. Es el nodo selecionado en el Grafo.
+    :param: input_destino. Referencia al elemento del input Destino para cambiarlo
+    """
+    # TODO Implement
+    # Nota: Debe de cambiar el nodo seleccionado en el Input de Destino.
+    pass
+
 
 
 if '__main__' == __name__:
